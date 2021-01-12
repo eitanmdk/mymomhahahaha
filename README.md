8b4f8f57e442a3309446b856a1ce316f43ad5920
eitanmdk <72807177+eitanmdk@users.noreply.github.com>
2021-01-11 19:46:06 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,46 @@
+# Nitro Sniper [![CodeFactor](https://www.codefactor.io/repository/github/slow/nitro-sniper/badge)](https://www.codefactor.io/repository/github/slow/nitro-sniper) [![GitHub issues](https://img.shields.io/github/issues/slow/nitro-sniper?style=flat)](https://github.com/slow/nitro-sniper/issues) [![GitHub stars](https://img.shields.io/github/stars/slow/nitro-sniper?style=flat)](https://github.com/slow/nitro-sniper/stargazers)
+Snipe nitro gift codes from alt accounts to your main account.
+
+# Preview 
+![Preview](https://i.imgur.com/PU3QaZc.png)
+![Webhook](https://i.imgur.com/R9TQZ0k.png)
+
+# Features
+- Colours for your eyes to distinguish success and failure.
+- Multi-token support.
+- Mobile notifications work if you don't use your main token for sniping.
+- Removes non-alphanumeric chars automatically from codes and tries to redeem.
+- Auto-detects obvious fake codes.
+- Light, no cluttered code, very simple and efficient.
+- One-click deploy.
+- Webhook support.
+
+# Servers
+- It is recommended to join servers from server sites such as disboard.
+- Avoid a huge quantity of big servers with **50,000** members or more as it can slow down the sniper due to it using a lot more memory for its cache.
+- Recommended site: https://disboard.org/servers?sort=-member_count
+
+# Installation methods
+## ___Heroku (recommended)___
+Click on the image below and login to continue the setup. 
+
+##### After deploying, Go to the `Resources` tab and turn off the `web` dyno and turn on `worker`. Now you can go ahead and click on `More` in the top right and click on logs.
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/slow/nitro-sniper/tree/main)  
+
+## ___Local___
+- Make sure [Node](https://nodejs.org/en/) is installed on your system and open a command prompt/terminal.
+- Run `git clone https://github.com/slow/nitro-sniper nitro-sniper`
+- Run `cd nitro-sniper`
+- Run `npm install`
+- Edit the dotenv file. <br>
+    - To insert multiple tokens in the guildTokens variable, use `,` as a separator.<br>
+    - Example: `token1,token2`
+- Run `node .`
+
+# Tips
+- The less the latency to discord servers, the better; You could be competing with other snipers. <br>
+    - High bandwidth hosting would benefit the sniper.
+- There's a really high risk you might get terminated using this. <br>
+    - Do not mention you have this anywhere. <br>
+    - This is technically stealing money from discord.
---
+++ index.js
@@ -0,0 +1,116 @@
+const userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36';
+const regex = new RegExp(/(discord\.gift\/|discord\.com\/gifts\/|discordapp\.com\/gifts\/)[^\s]+/gim);
+const consumedCodes = [];
+
+require('dotenv').config({ path: 'dotenv' });
+
+const phin = require('phin').unpromisified;
+const { Client, WebhookClient, RichEmbed } = require('discord.js');
+const chalk = require('chalk');
+
+const tokens = process.env.guildTokens.split(',');
+const mainToken = process.env.mainToken;
+const webhookUrl = process.env.webhookUrl;
+
+if (webhookUrl != null) {
+   const webhooktoken = /[^/]*$/.exec(webhookUrl)[0];
+   const webhookid = webhookUrl.replace(/^.*\/(?=[^\/]*\/[^\/]*$)|\/[^\/]*$/g, '');
+   const webhookclient = new WebhookClient(webhookid, webhooktoken);
+   function fireHook(type, code, guild, author, time, alt) {
+      let embed = new RichEmbed()
+         .setColor('#41FC9F')
+         .setTitle('Nitro Sniped')
+         .addField('Time Taken', time, true)
+         .addField('Type', type, true)
+         .addField('Code', code, true)
+         .addField('Account', alt, true)
+         .addField('Author', `${author}`, true)
+      guild ?
+         embed.addField('Location', `${guild} (Server)`, true) :
+         embed.addField('Location', `DMs`, true);
+      webhookclient.send('', {
+         embeds: [embed]
+      }).catch(() => {
+         if (webhookUrl) {
+            console.log(
+               chalk.red('[Sniper] Couldn\'t reach webhook. Your webhook URL is invalid.')
+            )
+         }
+      })
+   }
+}
+
+for (const token of tokens) {
+   const client = new Client({
+      messageCacheLifetime: 1,
+      messageCacheMaxSize: 10,
+      messageSweepInterval: 5,
+      messageEditHistoryMaxSize: 1,
+      restTimeOffset: 0,
+      disabledEvents: [
+         'TYPING_START', 'VOICE_SERVER_UPDATE', 'RELATIONSHIP_ADD', 'RELATIONSHIP_REMOVE',
+         'GUILD_ROLE_DELETE', 'GUILD_ROLE_UPDATE', 'GUILD_BAN_ADD', 'GUILD_BAN_REMOVE',
+         'CHANNEL_UPDATE', 'CHANNEL_PINS_UPDATE', 'MESSAGE_DELETE', 'MESSAGE_UPDATE',
+         'MESSAGE_DELETE_BULK', 'MESSAGE_REACTION_ADD', 'MESSAGE_REACTION_REMOVE',
+         'GUILD_MEMBER_UPDATE', 'GUILD_MEMBERS_CHUNK', 'GUILD_ROLE_CREATE',
+         'MESSAGE_REACTION_REMOVE_ALL', 'USER_UPDATE', 'USER_NOTE_UPDATE',
+         'USER_SETTINGS_UPDATE', 'PRESENCE_UPDATE', 'VOICE_STATE_UPDATE',
+         'GUILD_UPDATE', 'GUILD_MEMBER_ADD', 'GUILD_MEMBER_REMOVE'
+      ]
+   });
+
+   client.on('message', async (msg) => {
+      let codes = msg.content.match(regex);
+      if (!codes || !codes.length) return;
+      for (let code of codes) {
+         code = code.replace(/(discord\.gift\/|discord\.com\/gifts\/|discordapp\.com\/gifts\/)/gim, '').replace(/\W/g, '');
+
+         let start = new Date();
+
+         if (consumedCodes.includes(code)) {
+            console.log(chalk.gray(`[Sniper] Avoiding Duplicate - Code: ${chalk.bold(code)} - ${msg.guild ? msg.guild.name : 'DMs'} (${msg.author.tag})`));
+            continue;
+         }
+
+         if (code.length < 16 || code.length > 24) {
+            console.log(chalk.gray(`[Sniper] Fake Code - Code: ${chalk.bold(code)} - ${msg.guild ? msg.guild.name : 'DMs'} (${msg.author.tag})`));
+            continue;
+         }
+
+         phin({
+            url: `https://discord.com/api/v6/entitlements/gift-codes/${code}/redeem`,
+            method: 'POST',
+            parse: 'json',
+            headers: {
+               'Authorization': mainToken,
+               'User-Agent': userAgent
+            }
+         }, (err, res) => {
+            let end = `${new Date() - start}ms`;
+            if (err) {
+               return console.log(chalk.red(`[Sniper] Error - Code: ${chalk.bold(code)} - ${err} - ${msg.guild ? msg.guild.name : 'DMs'} (${msg.author.tag}) - ${end}`));
+            } else if (res.body.message === '401: Unauthorized') {
+               return console.log(chalk.red(`[Sniper] Error - Code: ${chalk.bold(code)} - Your main token is invalid.`));
+            } else if (res.body.message == 'This gift has been redeemed already.') {
+               console.log(chalk.red(`[Sniper] Already Redeemed - Code: ${chalk.bold(code)} - ${msg.guild ? msg.guild.name : 'DMs'} (${msg.author.tag}) - ${end}`));
+            } else if ('subscription_plan' in res.body) {
+               console.log(chalk.green(`[Sniper] Success - Code: ${chalk.bold(code)} - ${res.body.subscription_plan.name} - ${msg.guild ? msg.guild.name : 'DMs'} (${msg.author.tag}) - ${end}`));
+               fireHook(res.body.subscription_plan.name, code, msg.guild ? msg.guild.name : null, msg.author.tag, end, client.user.tag);
+            } else if (res.body.message == 'Unknown Gift Code') {
+               console.log(chalk.red(`[Sniper] Invalid Code - Code: ${chalk.bold(code)} - ${msg.guild ? msg.guild.name : 'DMs'} (${msg.author.tag}) - ${end}`));
+            }
+            consumedCodes.push(code);
+         });
+      }
+   });
+
+   client.on('ready', () => {
+      console.log(chalk.green(`[Sniper] Logged in as ${client.user.tag}.`));
+   });
+
+   setTimeout(() => {
+      client.login(token).catch(err => {
+         console.log(chalk.red(`[Sniper] Skipping alt token - ${err} - ${token}`))
+      });
+   }, Math.floor(Math.random() * (3000 - 1000 + 1)) + 1000);
+}
